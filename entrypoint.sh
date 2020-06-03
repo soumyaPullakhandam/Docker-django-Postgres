e28f93eac797794652aa50887fa95ffc4fca6204
Soumya <soumya.pullakhandam1@gmail.com>
2020-06-03 10:40:46 +0200

    added entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+cd hello_django
+python manage.py collectstatic --no-input
+python manage.py migrate
+cd ..
+
+exec "$@"
